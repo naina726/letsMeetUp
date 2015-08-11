dda449046ec6f6586793cd85650937c885c29bc9
Naina Prasad <prasad.naina@gmail.com>
2015-08-11 12:09:53 -0400

    Added YELP Client initialization in yelp.rb

---
+++ LetsMeetUp_app/app/models/yelp.rb
@@ -0,0 +1,14 @@
+require 'yelp' 
+class YELP
+  Yelp.client.configure do |config|
+	  config.consumer_key = ENV["YELP_CONS_KEY"]
+	  config.consumer_secret = ENV["YELP_CONS_SECRET"]
+	  config.token = ENV["YELP_TOKEN"]
+	  config.token_secret = ENV["YELP_TOKEN_SECRET"]
+  end
+
+end
+
+
+
+
