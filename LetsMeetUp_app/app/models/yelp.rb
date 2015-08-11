888eccba6bfa505aff075b6aa56cb346f34c5098
Naina Prasad <prasad.naina@gmail.com>
2015-08-11 14:06:35 -0400

    YELP AUTH WORKING YAAAAS

--- LetsMeetUp_app/app/models/yelp.rb
+++ LetsMeetUp_app/app/models/yelp.rb
@@ -1,14 +1,10 @@
 require 'yelp' 
 class YELP
-  Yelp.client.configure do |config|
-	  config.consumer_key = ENV["YELP_CONS_KEY"]
-	  config.consumer_secret = ENV["YELP_CONS_SECRET"]
-	  config.token = ENV["YELP_TOKEN"]
-	  config.token_secret = ENV["YELP_TOKEN_SECRET"]
-  end
+  # Yelp.client.configure do |config|
+	 #  config.consumer_key = ENV["YELP_CONS_KEY"]
+	 #  config.consumer_secret = ENV["YELP_CONS_SECRET"]
+	 #  config.token = ENV["YELP_TOKEN"]
+	 #  config.token_secret = ENV["YELP_TOKEN_SECRET"]
+  # end
 
 end
-
-
-
-
