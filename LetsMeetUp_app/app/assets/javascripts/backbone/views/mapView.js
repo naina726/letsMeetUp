b5f9a2ae57a374a7736d665b58ee1a79b7b8f112
Hwan Ho Jeoung <ahj212@gmail.com>
2015-08-12 15:47:41 -0400

    started google maps view

--- LetsMeetUp_app/app/assets/javascripts/backbone/views/mapView.js
+++ LetsMeetUp_app/app/assets/javascripts/backbone/views/mapView.js
@@ -2,5 +2,14 @@ App.Views.MapView = Backbone.View.extend({
 	el: 'map',
 	initialize: function() {
 		console.log("NEW Map VIEW CREATED");
-	}
+			var mapCanvas = $('#map');
+			var mapOptions = {
+				center: new google.maps.LatLng(44.5403, -78.5463),
+				zoom: 8,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(mapCanvas, mapOptions);
+	},
+	google.maps.event.addDomListener(window, 'load', initialize);
+
 });
--- LetsMeetUp_app/app/models/yelp.rb
+++ LetsMeetUp_app/app/models/yelp.rb
@@ -1,12 +1,15 @@
 require 'yelp' 
+require 'pry'
 class YELP
 
 	def self.search(lat1, long1, lat2, long2, activity)
 		#COMPUTE MIDPOINT OF LOCATIONS
 		avgLat = (lat1+lat2)/2
+		p avgLat
 		avgLong = (long1+long2)/2
+		p avgLong
 		coords = { latitude: avgLat, longitude: avgLong }
-
+		binding.pry
 
 		params = {
 			term: "activity"
