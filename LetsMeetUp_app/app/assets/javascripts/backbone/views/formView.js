8828217814155d8e0e79d83be0b195d251451f9c
Hwan Ho Jeoung <ahj212@gmail.com>
2015-08-13 10:29:04 -0400

    possible merge conflict

--- LetsMeetUp_app/app/assets/javascripts/backbone/views/formView.js
+++ LetsMeetUp_app/app/assets/javascripts/backbone/views/formView.js
@@ -1,51 +1,57 @@
 App.Views.FormView = Backbone.View.extend({
-	el: '#form',
-	initialize: function(){
-		console.log("RENDERING FORM VIEWZ");
-		this.template = HandlebarsTemplates["form"];
-		console.log(this.template)
-		// var compiled_html = this.template();
-		// console.log(compiled_html);
-
-		// this.$(el).append( compiled_html);
-		this.render();
-	},
-	render: function() {
-		this.$el.html(this.template());
-	},
-	events: {
-		'click button': 'getInfoFromForm'
-	},
-	getInfoFromForm: function(){
-		var add1 = $('#addressOne').val();
-		var add2 = $('#addressTwo').val();
-		var activity = $("#searchActivity").val();
+    el: '#form',
+    initialize: function(){
+        console.log("RENDERING FORM VIEWZ");
+        this.template = HandlebarsTemplates["form"];
+        console.log(this.template)
+        this.render();
+    },
+    render: function() {
+        this.$el.html(this.template());
+    },
+    events: {
+        'click button': 'convert'
+    },
+    convert: function(){
+        var add1 = $('#addressOne').val();
+        var add2 = $('#addressTwo').val();
+        this.activity = $("#searchActivity").val();
 
         var geocoder1 = new google.maps.Geocoder();
         geocoder1.geocode( { 'address': add1}, function(results, status) {
-        	if (status == google.maps.GeocoderStatus.OK) {
-           		var lat1 = results[0].geometry.location.lat();
-           		var long1 = results[0].geometry.location.lng();
-         	} 
-		});
+            if (status == google.maps.GeocoderStatus.OK) {
+                   lat1 = results[0].geometry.location.lat();
+                   long1 = results[0].geometry.location.lng();
+             } 
+        });
         var geocoder2 = new google.maps.Geocoder();
         geocoder2.geocode( { 'address': add2}, function(results, status) {
-        	if (status == google.maps.GeocoderStatus.OK) {
-           		var lat2 = results[0].geometry.location.lat();
-           		var long2 = results[0].geometry.location.lng();
-         	} 
-		});
-	}
-})
-//ajax post to search route in controller
-//send as data
+            if (status == google.maps.GeocoderStatus.OK) {
+                   this.lat2 = results[0].geometry.location.lat();
+                   this.long2 = results[0].geometry.location.lng();
+             } 
+        });
+        console.log(this.lat1 + "     " + this.long1);
 
-// $.ajax({
-// 	type: "POST",
-// 	url: '/whatever',
-// 	data: {long: whatever, lat: whatever },
-//}).done(function(data){
-	// App.mapView = new App.Views.MapView();
-	// App.mapView.generateMarkers(data);
-//})
+        this.internalSearch();
+    },
+    internalSearch: function(){
+        //ajax post to search route in controller
+        $.ajax({
+            type: "POST",
+            url: 'yelps/search',
+            data: {
+                lat1: this.lat1,
+                long1: this.long1,
+                lat2: this.lat2,
+                long2: this.long2,
+                activity: this.activity
+            }
+        }).done(function(data){
+            App.mapView = new App.Views.MapView();
+            App.mapView.generateMarkers(data);
+            //ALSO INITIALIZE LISTVIEW
+        })
+    }
+})
 
