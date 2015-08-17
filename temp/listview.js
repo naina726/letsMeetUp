838431cb9bd15e2fe8f09ed2334dec44721b2961
Connor Rowland <cjr6734@yahoo.com>
2015-08-17 19:16:07 -0400

    temp

--- temp/listview.js
+++ temp/listview.js
@@ -1,9 +1,17 @@
-$(function(){
-	$("button").on('click', function(){
-		for( var i = 0; i < 5; i++ ){
-			var newList = $("<div>").attr("id", "listResults");
-			var container = $("#listContainer");
-			newList.appendTo(container);	
-		}
-	})
-})
+// $(function(){
+// 	$("button").on('click', function(){
+// 		for( var i = 0; i < 5; i++ ){
+// 			var newList = $("<div>").attr("id", "listResults");
+// 			var container = $("#listContainer");
+// 			newList.appendTo(container);	
+// 		}
+// 	})
+// })
+
+
+
+$("#submit").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
